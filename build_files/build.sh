79b504c4cd1ac7f73cfb76b970532c0ef8f5485b
Hironio <269300764+Hironio@users.noreply.github.com>
2026-03-25 01:26:56 +0100

    Update build.sh
        skanpage \
        system-config-printer \
        virt-manager \
        libvirt-daemon-kvm \
        qemu-kvm \
        bridge-utils \
        wol

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Installazione pacchetti Dazzite (Brother, Virtualizzazione, Utility)
+dnf5 install -y \
+    skanpage \
+    system-config-printer \
+    virt-manager \
+    libvirt-daemon-kvm \
+    qemu-kvm \
+    bridge-utils \
+    wol
 
 # Use a COPR Example:
 #
